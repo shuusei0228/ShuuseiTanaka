2e9b622cd5230bfe793282087ab6d6bec4456f0a
ShuuseiTanaka <nob_nob1414@docomo.ne.jp>
2022-01-28 22:47:46 +0900

    create

--- app/Post.php
+++ app/Post.php
@@ -14,4 +14,8 @@ public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'ASC')->paginate($limit_count);
     }
-}    
+    protected $fillable = [
+    'title',
+    'body',
+];
+}
